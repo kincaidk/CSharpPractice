95b50f6ce17f61a88c099f598afe70adf12d4c8b
Kellen Kincaid <kellenceo@gmail.com>
2022-07-31 11:39:38 -0400

    Made a prompt for the user's name.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Why, hello there " + name + "!");
         }
     }
 }
