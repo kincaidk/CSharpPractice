11c651d4af9059dacafb2a00eff61a13cf22b059
Kellen Kincaid <kellenceo@gmail.com>
2022-07-31 13:50:01 -0400

    Filled out the Builder file.

--- Strings/Builder/Program.cs
+++ Strings/Builder/Program.cs
@@ -16,7 +16,37 @@ namespace Builder
             // Print basic stats about StringBuilder.
             Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
 
+            // Use Append to add strings to the StringBuilder.
+            sb.Append("The quick brown fox ");
+            sb.Append("jumps over the lazy dog");
+
+            // Use AppendLine to append a line ending
+            sb.AppendLine();
+
+            // Use AppendFormat to append formatted strings.
+            sb.AppendFormat("He did this {0} times", jumpCount);
+            sb.AppendLine();
+
+            // Use AppendJoin to iterate over a sequence of values.
+            sb.Append("He also jumped over ");
+            sb.AppendJoin(", ", animals);
+
+            // Use Replace to modify the text
+            sb.Replace("fox", "cat");
+
+            // Use Insert to add content at a desired index.
+            sb.Insert(2, "~INSERTED TEXT~");
             
+            // Print the string itself.
+            Console.WriteLine(sb.ToString());
+            // Print basic stats about StringBuilder again.
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+
+            //
+
+
+
+
         }
     }
 }
