5a42e02bc43030223261ef4d41e056d9e39c97f4
Kellen Kincaid <kellenceo@gmail.com>
2022-07-31 13:36:01 -0400

    Changed the StringBuilder folder to Builder because it was conflicting with the StringBuilder class.

---
+++ Strings/Builder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text; // StringBuilder class is in here.
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jumpCount = 10;
+            string[] animals = {"goats", "cats", "pigs"};
+
+            int maximumStringCapacity = 200;
+            StringBuilder sb = new StringBuilder("Initial string.", maximumStringCapacity);
+
+            // Print basic stats about StringBuilder.
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+
+            
+        }
+    }
+}
