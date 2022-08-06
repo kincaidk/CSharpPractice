5db3be5977986c5dda97b2c0ea122937d72734d7
Kellen Kincaid <kellenceo@gmail.com>
2022-08-06 10:18:00 -0400

    Added IsPalindromeFunction and dotnet console.

---
+++ IsPalindromeFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IsPalindromeFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
