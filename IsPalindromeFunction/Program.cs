82cfbbe559b004de2198078b20713ed84d2c8346
Kellen Kincaid <kellenceo@gmail.com>
2022-08-06 10:47:38 -0400

    Created IsPalindrome function

--- IsPalindromeFunction/Program.cs
+++ IsPalindromeFunction/Program.cs
@@ -4,9 +4,33 @@ namespace IsPalindromeFunction
 {
     class Program
     {
+        /*
+            Returns a bool representing whether input is a palindrome.
+            Returns an int representing the length of the input, if it is a palindrome. (0 otherwise)
+        */
+        static (bool,int) IsPalindrome(string input) {
+            bool inputIsPalindrome = true;
+            int inputLength = input.Length;
+            string upperCaseInput = input.ToUpper();
+
+            for (int i = 0; i < inputLength / 2; i++) {
+                if (upperCaseInput[i] != upperCaseInput[inputLength-1-i]) {
+                    inputIsPalindrome = false;
+                    break;
+                }
+            }
+
+            return (inputIsPalindrome, inputIsPalindrome ? inputLength : 0);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inputs = {"racecar", " racecar", "", " ", "test", "a.a", "aa", "Aa", "  "};
+
+            foreach (string input in inputs) {
+                var result = IsPalindrome(input);
+                Console.WriteLine($"Is '{input}' a palindrome? - {result.Item1}. Palindrome length - {result.Item2}");
+            }
         }
     }
 }
